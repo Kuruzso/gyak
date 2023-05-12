37aef64742cdc08ab0ad2061fa4ae7a498101dcf
Kuruzso <127405494+Kuruzso@users.noreply.github.com>
2023-05-12 09:41:51 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,42 +1,72 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using MySqlConnector;//NUGet-el feltelepítettük a mysql csomagot
+using MySqlConnector; //NuGet-tel feltelepítettük a MySqlConnector csomagot
 
-namespace gyak
+namespace adatbazis
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector; //NuGet-tel feltelepítettük a MySqlConnector csomagot
+
+namespace adatbazis
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            string connectionstring;//a kapcsolat létrehozásához szükséges adatok
-          
-            connectionstring = @"server=localhost;user=root;database=proba;";
-            MySqlConnection kapcsolat;//az adatbázissal kapcsolódunk(még egyéb műveletet nem végez)
-            kapcsolat = new MySqlConnection(connectionstring);
-            kapcsolat.Open();//Ha nem jók a kapcsolat létrehozásához adatok akkor itt áll le
-
-            string sql = "SELECT * FROM személy";//A futtatandó lekérdezés parancsa
-            MySqlCommand mSqlCmd = new MySqlCommand(sql, kapcsolat);//A parancs futtatásához szükséges objektum
-                                                                    //adott parancsot  akapcsolattal összeköti
-            MySqlDataReader adatok = mSqlCmd.ExecuteReader();//A parancsokat futtatjuk,
-                                                             //a keletkező adatokat tároljuk,
-                                                             //ahoz hasonló mint a strteamreader
-            while (adatok.Read())//Az adatok.read() beolvas egy rekordot (sort) az eredmény táblából
+            string connectionString; //A kapcsolat létrehozásához szükséges adatok
+            connectionString = @"server = localhost;user = root;database = proba";
+            MySqlConnection kapcsolat; //Az adatbázissal kapcsolódunk (még egyéb műveletet nem végez)
+            kapcsolat = new MySqlConnection(connectionString);
+            List<string> adatbazislista = new List<string>();
+            try
             {
-                Console.WriteLine(adatok[0]);//Az adatok utáni index jelzi,
-                                               //hogy hányadik mezőt kérem az eredménytáblából
+                kapcsolat.Open(); //Ha nem jók a kapcsolat létrehozásához szüséges adatok, akkor itt áll le,
+                                  //ha sikeres, akkor innentől elérhető az adatbázis
+                string sql = "SELECT * FROM személy"; //A futtatandó lekérdezés parancsa
+                MySqlCommand mSqlCmd = new MySqlCommand(sql, kapcsolat); //A parancs futtatásához szükséges objektum,
+
+                string nev = "sanyi";
+                string telepules = "peru";
+
+                MySqlCommand hozzaad = new MySqlCommand($"INSERT INTO `személy` (`nev`, `telepules`) VALUES('{nev}', '{telepules}')",kapcsolat);
+                hozzaad.ExecuteNonQuery();
+
+                //adott parancsot a kapcsolattal összeköti
+                MySqlDataReader adatok = mSqlCmd.ExecuteReader();//A parancsot futtatjuk,
+                                                                 //a keletkező adatokat tároljuk
+                                                                 //ahhoz hasonló, mint a StreamReader
+               
+
+                while (adatok.Read()) //Az adatok.Read() beolvas egy rekordot (sort) az eredménytáblából
+                {
+                    //összeállítok egy stringet az adatokból
+                    StringBuilder ujString = new StringBuilder();
+                    for (int i = 0; i < adatok.FieldCount; i++)
+                    {
+                        ujString.Append(adatok[i] + " ");
+                    }
+                    adatbazislista.Add(Convert.ToString(ujString)); //Az adatok utáni index jelzi,
+                                                                    //hogy hányadik mezőt kérem az eredménytáblából
+                }
+                for (int i = 0; i < adatbazislista.Count; i++)
+                {
+                    Console.WriteLine(adatbazislista[i]);
+                }
+                kapcsolat.Close(); //A műveletek befejezése után mindig zárjuk a kapcsolatot!
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Valami hiba történt!");
             }
 
-            kapcsolat.Clone();//a műveletek befejezése után mindig zárjuk a kapcsolatot!
-
-
-
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
+
